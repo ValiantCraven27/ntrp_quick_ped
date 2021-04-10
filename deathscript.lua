bfd997930b3f588a8b044d381600579f465e5a3c
Brwnlee <77315942+ValiantCraven27@users.noreply.github.com>
2021-04-10 09:48:21 -0400

    Add files via upload

--- deathscript.lua
+++ deathscript.lua
@@ -1,80 +1,37 @@
-IsDead = false
+--[[AddEventHandler("playerSpawned", function(spawn)
+          Citizen.Wait(1000)
+          exports.spawnmanager:setAutoSpawn(false)
+  
+end)]]
 
-AddEventHandler("playerSpawned", function(spawn)
-     Citizen.Wait(1000)
-     exports.spawnmanager:setAutoSpawn(false)
+Citizen.CreateThread(function()
+Citizen.Wait(1000)
+exports.spawnmanager:setAutoSpawn(false)
 end)
 
 Citizen.CreateThread(function()
-      while true do
-	Citizen.Wait(2000)
-                  StoreCoords  = GetEntityCoords(PlayerPedId())
- end
-end)
-
-Citizen.CreateThread(function()
-    while true do
-        Citizen.Wait(0)         
-    if IsEntityDead(PlayerPedId()) then
-        SetPlayerInvincible(PlayerPedId(), true)
-        IsDead = true
-        
-    if not IsEntityDead(PlayerPedId()) then 
-        SetPlayerInvincible(PlayerPedId(), false)
-        IsDead = false 
-     end        
-    end
-   end
- 
-end)
-
-Citizen.CreateThread(function()
-      while true do
-	Citizen.Wait(1)
-                                           if IsDead then
-                                                               
-                                                                                
-                                                                                DisableControlAction(0, 0, true) -- Changing view (V)
-					DisableControlAction(0, 22, true) -- Jumping (SPACE)
-					DisableControlAction(0, 23, true) -- Entering vehicle (F)
-					DisableControlAction(0, 24, true) -- Punching/Attacking
-					DisableControlAction(0, 29, true) -- Pointing (B)
-					DisableControlAction(0, 30, true) -- Moving sideways (A/D)
-					DisableControlAction(0, 31, true) -- Moving back & forth (W/S)
-					DisableControlAction(0, 37, true) -- Weapon wheel
-					DisableControlAction(0, 44, true) -- Taking Cover (Q)
-					DisableControlAction(0, 56, true) -- F9
-					DisableControlAction(0, 82, true) -- Mask menu (,)
-					DisableControlAction(0, 140, true) -- Hitting your vehicle (R)
-					DisableControlAction(0, 166, true) -- F5
-					DisableControlAction(0, 167, true) -- F6
-					DisableControlAction(0, 168, true) -- F7
-					DisableControlAction(0, 170, true) -- F3
-                                                                                DisableControlAction(0, 189, true) -- Handsup (X)
-					DisableControlAction(0, 288, true) -- F1
-					DisableControlAction(0, 289, true) -- F2
-					DisableControlAction(1, 323, true) -- Handsup (X)
-				  
+      while true do	
+                  local playerPed = GetPlayerPed(-1)
+            if not IsPlayerDead(playerPed) then
+                   RDX.ShowNotification("storing save coords", "info")  
+                   StoreCoords  = GetEntityCoords(PlayerPedId())                     
+                      --Citizen.Wait(15000)
   end
+   Citizen.Wait(60000)
  end
 end)
+
 
 RegisterCommand("revive",function()
-
-  if IsEntityDead(playerPed) then                      
-          Citizen.Wait(1000)
-          NetworkResurrectLocalPlayer( StoreCoords , 100.00, true, true, false )        
-          NTRP.Warp( StoreCoords )     
-          Citizen.Wait(2000)   
+local playerPed             = GetPlayerPed(-1)   
+local playerCoords       = GetEntityCoords(playerPed)
+  if IsEntityDead(playerPed) and GetEntityHealth(playerPed) < 1 then
+          DoScreenFadeOut(1000)      
+          RDX.ShowNotification("Debug 1")  
+          NetworkSetLocalPlayerInvincibleTime(5000)          
+          Citizen.Wait(5000)
+          NetworkResurrectLocalPlayer(StoreCoords, 100.00, true, true, false )              
+          RDX.ShowNotification("Debug 2")          
+          DoScreenFadeIn(1500)   
   end        
 end)
-
-NTRP = {}
-
-NTRP.Warp = function(coordinates)     
-          DoScreenFadeOut(1)              
-          Wait(2000)
-          SetEntityCoords(PlayerPedId(), coordinates, false, false, false, true) 
-          Wait(4000)
-          DoScreenFadeIn(2000)	
-end
