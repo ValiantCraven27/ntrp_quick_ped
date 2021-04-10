53246dd6025c3b1524139f5cee276b343d815930
Brwnlee <77315942+ValiantCraven27@users.noreply.github.com>
2021-04-10 09:55:45 -0400

    Update deathscript.lua

--- deathscript.lua
+++ deathscript.lua
@@ -13,8 +13,7 @@ Citizen.CreateThread(function()
       while true do	
                   local playerPed = GetPlayerPed(-1)
             if not IsPlayerDead(playerPed) then
-                   RDX.ShowNotification("storing save coords", "info")  
-                   StoreCoords  = GetEntityCoords(PlayerPedId())                     
+                  StoreCoords  = GetEntityCoords(PlayerPedId())                     
                       --Citizen.Wait(15000)
   end
    Citizen.Wait(60000)
@@ -27,11 +26,9 @@ local playerPed             = GetPlayerPed(-1)
 local playerCoords       = GetEntityCoords(playerPed)
   if IsEntityDead(playerPed) and GetEntityHealth(playerPed) < 1 then
           DoScreenFadeOut(1000)      
-          RDX.ShowNotification("Debug 1")  
           NetworkSetLocalPlayerInvincibleTime(5000)          
           Citizen.Wait(5000)
-          NetworkResurrectLocalPlayer(StoreCoords, 100.00, true, true, false )              
-          RDX.ShowNotification("Debug 2")          
+          NetworkResurrectLocalPlayer(StoreCoords, 100.00, true, true, false )                       
           DoScreenFadeIn(1500)   
   end        
 end)
